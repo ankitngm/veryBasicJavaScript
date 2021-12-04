97eecc45e0a782b0bd57558e9251ed9bd9d66b1b
ankitngm <95303933+ankitngm@users.noreply.github.com>
2021-12-04 23:54:02 +0530

    did some cleanup

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -4,17 +4,12 @@ let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
 
 
-console.log(countEl);
-
 let count = 0;
-
-console.log(count)
 
 
 function increment() {
     count = count + 1;
     countEl.textContent = count;
-    console.log(countEl);
 }
 
 function decrement() {
