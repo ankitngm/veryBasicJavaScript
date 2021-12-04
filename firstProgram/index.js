4485caac688aa99dcc234d4b5a8083570d3babb0
ankitngm <95303933+ankitngm@users.noreply.github.com>
2021-12-04 16:28:01 +0530

    decrement function added

--- firstProgram/index.js
+++ firstProgram/index.js
@@ -1,8 +1,27 @@
-let count = 0
+
+
+let countEl = document.getElementById('count-el');
+
+console.log(countEl);
+
+let count = 0;
+console.log(count);
+
 
 function increment() {
-    count = count + 1
-    console.log(count)
+    count = count + 1;
+    countEl.innnerHtml = count;
+    console.log(countEl);
 }
+
+function decrement() {
+    count = count - 1;
+    if(count<0)
+    count = 0;
+    countEl.innnerText = count;
+    console.log(countEl);
+}
+
+
 
 
